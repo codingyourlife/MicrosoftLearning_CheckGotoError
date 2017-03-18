081c0e94e93f2674007816c703d7295b34d7630b
hannesmayer <hannes.mayer@gpn-telecom.net>
2017-03-18 21:37:12 +0100

    user interaction added to application

--- IsErrorCode_MicrosoftLearning/Program.cs
+++ IsErrorCode_MicrosoftLearning/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IsErrorCode_MicrosoftLearning
+﻿namespace IsErrorCode_MicrosoftLearning
 {
-    class Program
+    using IsErrorCode_MicrosoftLearning.Interfaces;
+    using System;
+
+    public class Program
     {
+        public static IErrorCheckable ErrorCheck { get; set; }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Use refactored version (otherwise old one)? (y/n)");
+            var res = Console.ReadLine();
+
+            if(res.ToLower() == "y")
+            {
+                ErrorCheck = new ErrorCheckRefactored();
+            }
+            else
+            {
+                ErrorCheck = new ErrorCheck();
+            }
+
+            while (true)
+            {
+                Console.WriteLine("What number do you want to try?");
+
+                try
+                {
+                    var errorCode = Convert.ToInt32(Console.ReadLine());
+                    var returnedCodeAccepted = ErrorCheck.IsErrorCode(errorCode);
+                    Console.WriteLine(string.Format("IsErrorCode: {0}", returnedCodeAccepted ? "YES" : "NO"));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format("Here an ugly error msg: {0}", ex.Message));
+                }
+            }
         }
     }
 }
